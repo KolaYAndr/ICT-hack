37278d73137370a5bcadce7bf8671f14d116d5d8
KolaYAndr <nikolay_andreev02@mail.ru>
2022-05-21 12:28:24 +0300

    Всё заебись

--- tokenise.py
+++ tokenise.py
@@ -1,18 +1,15 @@
-import nltk
-from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.tokenize import word_tokenize
 import clearing_out as co
-
-nltk.download('stopwords')
-nltk.download('punkt')
-russian_stopwords = nltk.corpus.stopwords.words("russian")
+from rusenttokenize import ru_sent_tokenize
 
 
 def token_by_sentences(message):
-    tokenized = sent_tokenize(message)
+    tokenized = ru_sent_tokenize(message)
     return tokenized
 
 
 def token_by_words(message):
+    message = message.lower()
     message = co.clear_message(message)
     tokenized = word_tokenize(message)
     return tokenized
