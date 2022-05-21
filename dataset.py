a64e704ae71e0d5ca113bb8dbf970d79f3a5caa7
choma <chomakk09@gmail.com>
2022-05-21 16:38:01 +0300

    Новый датасет (3000)

--- dataset.py
+++ dataset.py
@@ -1,7 +1,9 @@
 #!python3 -m pip install vk_api
+import time
+
 import vk_api
 import requests
-import json
+
 import pandas as pd
 
 data_posts = []
@@ -14,7 +16,7 @@ def rq(off):
     count = 100
     owner_id = -34215577
 
-    for i in range(off, off + 900, 100):
+    for i in range(off, off + 3000, 100):
         req = requests.get(vk_config["domain"] + "wall.get", params={"access_token": vk_config["token"],
                                                                     "v": vk_config["version"],
                                                                      "account_id": vk_config["client_id"],
@@ -25,8 +27,7 @@ def rq(off):
         for item in data:
             data_posts.append(item['text'])
         off += count
+        time.sleep(0.5)
     return data_posts
 data_frame = pd.DataFrame({'texts': rq(0)})
 data_frame.to_csv('dataset.csv')
-
-
