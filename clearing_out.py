7f3bc98088792340ebda621a2740f7f134dd73bb
KolaYAndr <nikolay_andreev02@mail.ru>
2022-05-21 11:18:18 +0300

    Очистил текст от пунктуации

---
+++ clearing_out.py
@@ -0,0 +1,8 @@
+import string
+
+def clear_message(message):
+    string.punctuation
+    clean_text = message
+    for ch in string.punctuation:
+        clean_text = clean_text.replace(ch, "")
+    return clean_text
--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import clearing_out as co
 
 
+clear = co.clear_message("Я ебал вас, говнари!")
+print(clear)
+
+
+
+# def get_message(message):
+#     return message
 
