26b8b1a74239f867a9fb06804ca137d2e8956d02
KolaYAndr <nikolay_andreev02@mail.ru>
2022-05-21 14:31:46 +0300

    Препроцесс готов

--- tokenise.py
+++ tokenise.py
@@ -1,6 +1,9 @@
 from nltk.tokenize import word_tokenize
 from rusenttokenize import ru_sent_tokenize
+import nltk
 
+nltk.download('stopwords')
+nltk.download('punkt')
 
 def token_by_sentences(message):
     tokenized = ru_sent_tokenize(message)
