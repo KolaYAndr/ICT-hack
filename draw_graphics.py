9d30a038eb566bca245f5312408eb4c91812a23a
KolaYAndr <nikolay_andreev02@mail.ru>
2022-05-21 18:00:49 +0300

    Научились рисовать графики

--- draw_graphics.py
+++ draw_graphics.py
@@ -7,7 +7,7 @@ def draw_barplots(dictionary):
     df = pd.DataFrame(dictionary).T
     # выкинем столбец skip, он всё равно ничего полезного не делает
     df = df.drop(labels='skip', axis=1)
-    df = df.sum()
+    df = df.count()
     fig, axis = plt.subplots(1, 1, figsize=(16, 8))
     sns.barplot(ax=axis, y=df.values, x=df.index, palette="Set1")
     plt.show()
